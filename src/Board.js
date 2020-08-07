2878a9d16283992fbbb3d880efc8efdf69c554a8
bharat_patil <patilbharat09@gmail.com>
2020-08-08 00:20:25 +0530

    Added basic Chess  layout

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,36 @@ import React from "react";
 import Square from "./Square";
 import Knight from "./Knight";
 
-export default function Board() {
+function renderSquare(i, [knightX, knightY]) {
+  const x = i % 8;
+  const y = Math.floor(i / 8);
+  const isKnightHere = x === knightX && y === knightY;
+  const black = (x + y) % 2 === 1;
+  const piece = isKnightHere ? <Knight /> : null;
+
   return (
-    <div>
-      <Square black>
-        <Knight />
-      </Square>
+    <div key={i} style={{ width: "12.3%", height: "12vh", border: "1px solid" }}>
+      <Square black={black}>{piece}</Square>
+    </div>
+  );
+}
+
+export default function Board({ knightPosition }) {
+  const squares = [];
+  for (let i = 0; i < 64; i++) {
+    squares.push(renderSquare(i, knightPosition));
+  }
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {squares}
     </div>
   );
 }
